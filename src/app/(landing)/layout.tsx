489ba23d020441f073cb0033a88ac928ec5ddff0
joelsilva333 <joelpitra44@gmail.com>
2025-09-25 14:57:50 +0100

    remove image from layout and enhance home page structure

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,16 +1,3 @@
-import Image from "next/image"
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-	return (
-		<div className="relative w-full min-h-screen">
-			<Image
-				src={"/images/Me.png"}
-				alt={"Me"}
-				width={1920}
-				height={1080}
-				className="w-full h-screen object-cover"
-			/>
-			{children}
-		</div>
-	)
+	return <div className="w-full min-h-screen overflow-hiddens">{children}</div>
 }
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image"
+
 export default function Home() {
 	return (
-		<div>
-			<h1>Home Page</h1>
+		<div className="flex flex-col items-center relative justify-center w-full h-screen">
+			<div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black bg-opacity-50 text-white">
+				<Image
+					src={"/images/Me.png"}
+					alt={"Me"}
+					width={1920}
+					height={1080}
+					className="w-full h-screen object-cover"
+				/>
+			</div>
 		</div>
 	)
 }
